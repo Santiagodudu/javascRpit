87c0f0a85f554c54e06f9f8d27a26b069a4885cb
santiago_dudu <magestieduardo@gmail.com>
2022-08-20 18:11:22 -0300

    git push

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,4 +1,22 @@
 function contagem() {
-    var inicio = document.getElementById("inicio")
-    var passo = document.getElementById("passo")
+    var inicio = Number(document.getElementById("inicio").value)
+    var fim = Number(document.getElementById("fim").value)
+    var passo = Number(document.getElementById("passo").value)
+    var res = document.querySelector("div#res")
+
+    if (inicio == 0 || fim == 0){
+        res.innerHTML = "Impossível contar."
+    }
+    else {
+        if (passo == 0) {
+        alert("Passo inválido! Considerando PASSO 1")
+        passo = 1
+        }
+        res.innerHTML = "Contando: <br>"
+
+        for(let i = inicio ; i <= fim ; i+= passo){
+            res.innerHTML += `${i}👉`
+        }
+        res.innerHTML += `🏁`
+    }
 }
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert("olá")
+function tabuada() {
+    let num = document.getElementById("txtn")
+    let tab = document.getElementById("seltab") 
+    if (num.value.length == 0) {
+        window.alert("Por favor, digite um número")
+    }
+    else {
+    num = Number(num.value)
+    tab.innerHTML = ""
+    for(let i = 0 ; i <= 10 ; i++) {
+        let item = document.createElement("option")
+        item.text = `${num} * ${i} = ${num * i}`
+        item.value = `tab${i}`
+        tab.appendChild(item)
+    }
+    }
+}
